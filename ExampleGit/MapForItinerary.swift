efdad88a7a03bf4bd33f8cb69f2dcac6fb60c336
Padma Anusha Vidapanakal <vidapanakal.anusha@gmail.com>
2018-03-12 14:03:49 +0530

    Add new map-for-itinerary feature

---
+++ ExampleGit/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  ExampleGit
+//
+//  Created by Padma Anusha Vidapanakal on 12/03/18.
+//  Copyright © 2018 Padma Anusha Vidapanakal. All rights reserved.
+//
+
+import Foundation
